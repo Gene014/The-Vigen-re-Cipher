a0cc3a46b44d8253ec84198178d9277fdba37a42
Gene014 <calmaeugene06@gmail.com>
2023-04-16 20:09:57 +0800

    generates key, translate the generated key then encrypts the integer and getting the output, running but data act is looping

--- ciphercode.py
+++ ciphercode.py
@@ -12,6 +12,23 @@ while True:
         print("Welcome to The Vigenère Cipher, where your code is always protected!")
         message = input("Please enter your message. ").upper().replace(" ", "")
         keyword = input("Please enter your key. ").upper().replace(" ", "")
+        # generates key from keyword, extends the key until reached length of string
+        key = list(keyword)
+        if len(message) == len(key):
+            continue
+        else:
+            for i in range(len(message) - len(key)):
+                key.append(key[i % len(key)])
+        "" . join(key)
+                # tranlates the generated key to integers
+        key = [ord(i) - 65 for i in key]
+                # encrypts the integers
+        cipher_text = ""
+        for i in range(len(message)):
+            encrypted_num = (ord(message[i]) - 65 + key[i % len(key)]) % 26
+            cipher_text += chr(encrypted_num + 65)
+                # output the cipher text
+        print(f"Hi {account_name}! Here is your ciphertext: {cipher_text}")
     elif user_input.lower() == 'no':
         print("Terminating Program...")
         exit()
