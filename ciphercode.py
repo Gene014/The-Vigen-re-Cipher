c5d07dac95df6fc9b961c70d38843c3567cd722e
Gene014 <calmaeugene06@gmail.com>
2023-04-16 19:15:43 +0800

    input string variables

---
+++ ciphercode.py
@@ -0,0 +1,6 @@
+# CALMA, Eugene Marie S.
+# BSCPE_1-4
+# OOP Laboratory Exercise Number 2 Problem 3 (The Vigenère Cipher)
+
+# input and inserting your acount name
+account_name = input("Hi, Good day! Please enter your name.")
